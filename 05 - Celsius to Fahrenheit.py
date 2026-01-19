01219d217a20a007848f1a7de1fe1112cd005c20
Luis Enrique <lleonbola2gmail.com>
2026-01-19 03:17:43 -0300

    01/19/2026

--- 05 - Celsius to Fahrenheit.py
+++ 05 - Celsius to Fahrenheit.py
@@ -1,5 +1,13 @@
-## Clean Code: Cumple con el Principio de Responsabilidad Única (hace una sola cosa)
-def bienvenida():
-    print('Hello World')
+# CLEAN CODE: Uso de Constantes (Screaming Snake Case) para evitar "Magic Numbers".
+# Facilita el mantenimiento si las constantes de conversión necesitaran cambiar.
 
-bienvenida()
+FREEZING_POINT_F = 32
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
+
+def convert_celsius_to_fahrenheit(celcius: float) -> float:
+    return (celcius * CELSIUS_TO_FAHRENHEIT_FACTOR) + FREEZING_POINT_F
+
+temperature_in_celsius = 20.0
+result = convert_celsius_to_fahrenheit(temperature_in_celsius)
+
+print(f"{temperature_in_celsius}°C es igual a {result}°F")
